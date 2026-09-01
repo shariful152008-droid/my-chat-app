25e2eadffb7851f75db75e19ceeba1f4e7948b07
shariful152008-droid <shariful152008@gmail.com>
2026-09-01 07:00:24 +0600

    fixed-backticks

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-   console.log(Server is running on port ${PORT});
+server.listen(PORT, () => {    
+     console.log(`Server is running on port ${PORT}`);
 });
