ed922925e1912d1aa88f25e8eb953fd1556f50d7
shariful152008-droid <shariful152008@gmail.com>
2026-09-01 06:52:50 +0600

    final-syntax-fix

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(সার্ভার সফলভাবে সচল হয়েছে পোর্ট: ${PORT});
+   console.log(Server is running on port ${PORT});
 });
